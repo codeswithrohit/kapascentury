5f9afe969a9c076d797b057f795cc2c359d39b62
Ashutosh Singh Kushwaha <onlyashu20799@gmail.com>
2026-03-16 17:28:44 +0530

    add order list and product list

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -1,59 +1,49 @@
-import Sidebar from "../../src/adminPanel/Sidebar";
-import Navbar from "../../src/adminPanel/Navbar";
-import StatCard from "../../src/adminPanel/StatCard.jsx";
-import PerformanceChart from "../../src/adminPanel/PerformanceChart";
-import ConversionChart from "../../src/adminPanel/ConversionChart";
-import MapSection from "../../src/adminPanel/MapSection";
-import TopPagesTable from "../../src/adminPanel/TopPagesTable";
-import OrdersTable from "../../src/adminPanel/OrdersTable";
+"use client";
+
+import StatCard from "../../src/adminPanel/dashboardPages/StatCard";
+import PerformanceChart from "../../src/adminPanel/dashboardPages/PerformanceChart";
+import ConversionChart from "../../src/adminPanel/dashboardPages/ConversionChart";
+import MapSection from "../../src/adminPanel/dashboardPages/MapSection";
+import TopPagesTable from "../../src/adminPanel/dashboardPages/TopPagesTable";
+import OrdersTable from "../../src/adminPanel/dashboardPages/OrdersTable";
+import Layout from "../../src/adminPanel/Layout";
 
 export default function Dashboard() {
+  return (
+    <Layout>
+      
+      <div className="space-y-6">
+        
+        {/* Alert */}
+        <div className="bg-orange-100 text-orange-700 p-3 rounded">
+          We regret to inform you that our server is currently experiencing technical difficulties.
+        </div>
 
-return (
+        {/* Stats */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <StatCard title="Total Orders" value="13,647" growth="+2.3%" />
+          <StatCard title="New Leads" value="9,526" growth="+8.1%" />
+          <StatCard title="Deals" value="976" growth="-0.3%" />
+          <StatCard title="Booked Revenue" value="$123.6k" growth="-10.6%" />
+        </div>
 
-<div className="flex bg-gray-100 min-h-screen">
+        {/* Charts */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <PerformanceChart />
+          <ConversionChart />
+        </div>
 
-{/* SIDEBAR */}
-<Sidebar />
+        {/* Sessions + Top Pages */}
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+          <MapSection />
+          <TopPagesTable />
+        </div>
 
-{/* MAIN CONTENT */}
-<div className="flex-1 ml-64">
+        {/* Orders */}
+        <OrdersTable />
 
-<Navbar/>
+      </div>
 
-<div className="p-6 space-y-6">
-
-<div className="bg-orange-100 text-orange-700 p-3 rounded">
-We regret to inform you that our server is currently experiencing technical difficulties.
-</div>
-
-<div className="grid md:grid-cols-4 gap-4">
-
-<StatCard title="Total Orders" value="13,647" growth="+2.3%" />
-
-<StatCard title="New Leads" value="9,526" growth="+8.1%" />
-
-<StatCard title="Deals" value="976" growth="-0.3%" />
-
-<StatCard title="Booked Revenue" value="$123.6k" growth="-10.6%" />
-
-</div>
-
-<PerformanceChart />
-
-<ConversionChart />
-
-<MapSection />
-
-<TopPagesTable />
-
-<OrdersTable />
-
-</div>
-
-</div>
-
-</div>
-
-);
+    </Layout>
+  );
 }
